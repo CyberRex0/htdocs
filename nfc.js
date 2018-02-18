7d7f7242836941a91d5b7d2744232a1a1cdfcfd1
SquidSky <hspwinx86@gmail.com>
2018-02-18 10:06:17 +0900

    AmiiboReaderEmulator デモ用

--- nfc.js
+++ nfc.js
@@ -7,12 +7,15 @@ function setstatus(text) {
 function setresult(text) {
  $("res").innerHTML = text;
 }
+function setEmulateStatus(text) {
+  $("emulatestat").innerHTML = text;
+}
 
 function init() {
- if(!window.amiibo) {
+ /*if(!window.amiibo) {
   alert("WiiUブラウザからアクセスしてください");
   return;
- }
+ }*/
 
  window.amiibo.startSearchAmiibo(); //Amiibo検索開始
  setstatus("Amiibo読み込み準備完了");
@@ -37,49 +40,9 @@ function tagLost(e) {
 }
 
 window.onload = function () {
- if(typeof window.amiibo === "undefined") initAmiiboEmulator(); //WiiUでない場合はAmiiboエミュレータ初期化
+ if(typeof window.amiibo === "undefined") {
+   initAmiiboEmulator(); //WiiUでない場合はAmiiboエミュレータ初期化
+   setEmulateStatus("はい");
+ }
  init(); //アプリケーション初期化
-}
-
-//Amiibo reader emulate
-function initAmiiboEmulator() {
-  window.amiibo = function() {
-    console.log("Amiibo API is emulated");
-  }
-  //window.amiiboに同じ関数名を登録
-  window.amiibo.startSearchAmiibo = function() {
-    //Amiibo検索開始をエミュレート
-    //console.log("Amiibo emulator: Start search emulate");
-    var cev = document.createEvent("HTMLEvents");
-    cev.initEvent("AmiiboTagSearchStart", true, false);
-    window.dispatchEvent(cev);
-  }
-  window.amiibo.endSearchAmiibo = function() {
-    //Amiibo検索終了をエミュレート
-    var cev = document.createEvent("HTMLEvents");
-    cev.initEvent("AmiiboTagSearchCancel", true, false);
-    window.dispatchEvent(cev);
-  }
-  window.amiibo.playAmiiboSE = function() {
-    //「アミィーボッ！」をエミュレート
-    var snd = new Audio();
-    snd.src = "audio/amiibo.mp3";
-    snd.load();
-    snd.play();
-  }
-  window.amiibo.emulateAmiiboLoaded = function() {
-    var cev = document.createEvent("HTMLEvents");
-    cev.initEvent("AmiiboTagDetected", true, false);
-    window.customEvent = {
-      tag: {
-        info: {
-          id: ['0x22201','0x20108','0x82177'],
-          emulateinfo: 'yes'
-        },
-        isRead: true
-      }
-    };
-    window.dispatchEvent(cev);
-    console.log("Emulate amiibo tag load");
-  }
 }
